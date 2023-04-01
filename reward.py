6d669182538c92c13da5f8ea7c380e6413eff541
Carl Björknäs <carl.bjorknas@gmail.com>
2023-04-01 08:30:21 +0200

    First idea for my own reward algorithm.

---
+++ reward.py
@@ -0,0 +1,6 @@
+# Idé: 
+# Kombinera nedanstående för högsta belöning
+# I första hand, bilen ska vara hyfsat i mitten
+# I andra hand, bilen ska vara på väg åt rätt håll
+# I tredje hand, bilen ska ha hög hastighet
+
