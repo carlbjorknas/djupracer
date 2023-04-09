f1915786430e61e38c44fd9cc4f23716c755f62a
Carl Björknäs <carl.bjorknas@gmail.com>
2023-04-10 00:21:15 +0200

    Punished more. Rewarded for turning right direction.

--- Punish.py
+++ Punish.py
@@ -7,7 +7,8 @@ def reward_function(params):
 
     distance_from_center = params['distance_from_center']
     speed = params['speed']    
-    heading = params['heading']      
+    heading = params['heading']  
+    steering_angle = params['steering_angle']
     
     waypoints = params['waypoints']   
     closest_waypoints = params['closest_waypoints']   
@@ -20,7 +21,7 @@ def reward_function(params):
     off_track = params['is_offtrack']
 
     if off_track:
-        return -5.0
+        return -50.0
 
     ## reward for position
     pos_reward = 1.0
@@ -32,17 +33,37 @@ def reward_function(params):
     ## reward for speed
     speed_reward = speed / 4
 
-    ## reward for heading
+    ## reward for direction
+    direction_reward = calc_direction_reward(car_x, car_y, heading, steering_angle, next_waypoint[0], next_waypoint[1])
+
+    return 0.5 * direction_reward + 0.4 * pos_reward + 0.1 * speed_reward
+
+def calc_direction_reward(car_x, car_y, heading, steering_angle, next_x, next_y):
+
     # Calculate the direction to next waypoint in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    next_waypoint_direction_from_car = math.degrees(math.atan2(next_waypoint[1] - car_y, next_waypoint[0] - car_x))
+    next_waypoint_direction_from_car = math.degrees(math.atan2(next_y - car_y, next_x - car_x))
 
     # Calculate the difference between the direction to the next waypoint and the heading direction of the car
     direction_reward = 0.0
-    direction_diff = abs(next_waypoint_direction_from_car - heading)
+    direction_diff = next_waypoint_direction_from_car - heading
     if direction_diff > 180:
-        direction_diff = 360 - direction_diff
+        direction_diff = direction_diff - 360
+    elif direction_diff < -180:
+        direction_diff = direction_diff + 360
 
-    if direction_diff < 45:
-        direction_reward = (45 - direction_diff) / 45
-
-    return 0.5 * direction_reward + 0.4 * pos_reward + 0.1 * speed_reward
+    if abs(direction_diff) < 45:
+        direction_reward = (45 - abs(direction_diff)) / 90 + 0.5
+    # Car is pointing to the right of the next waypoint but steers to the left    
+    elif direction_diff > 0: 
+        if steering_angle > 10:
+            direction_reward = 0.4
+        elif steering_angle > 0:
+            direction_reward = 0.2
+    # Car is pointing to the left of the next waypoint but steers to the right     
+    elif direction_diff < 0: 
+        if steering_angle < -10:
+            direction_reward = 0.4
+        elif steering_angle < 0:
+            direction_reward = 0.2
+        
+    return direction_reward
